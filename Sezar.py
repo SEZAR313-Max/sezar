1409955277208782e232d23d37efddfb1e246a10
SEZAR313-Max <59199711+SEZAR313-Max@users.noreply.github.com>
2019-12-26 18:33:00 +0430

    Sezar
    Sezar

---
+++ Sezar.py
@@ -0,0 +1,13 @@
+#SeZaR_Tech|#SeZaR
+print ("""
++===================================+
++============@SeZaR_TeCh============+
++=============@AH_SEZAR=============+
++===================================+
+""")
+a = input("Enter Your Text To Go")
+n = int(input('How much do you want your text to go: '))
+for i in range(n)
+print(n)
+print ("finish")
+print("@SeZaR_TeCh")
